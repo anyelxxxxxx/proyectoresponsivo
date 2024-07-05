d625be4d14d4d97a39d0bc9e03c042d71ec55ee4
anyelxxxxxx <sanyeli890@gmail.com>
2024-07-05 15:28:35 -0500

    tercer commit

--- js.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".toggle")
-const navbar = document.querySelector(".navbar")
-
-toggle.addEventListener("click", () =>{
-    navbar.classList.toggle("navbar_visible");
-});
